e0deccb87df784bd5c6327b37b367cbc02d30965
DuongCoder <gduongit9@gmail.com>
2020-12-03 15:46:40 +0700

    add getOis bug stream corrupted

--- src/client/LoginControl.java
+++ src/client/LoginControl.java
@@ -56,7 +56,7 @@ public class LoginControl {
                 InviteControl inviteControl = new InviteControl(clientControl, listFrm);
                 inviteControl.setUser((User) mesRecei.getObject());
                 listFrm.setVisible(true);
-                CheckMess checkMess = new CheckMess(clientControl.getClientSocket(),clientControl.ois );
+                CheckMess checkMess = new CheckMess(clientControl.getClientSocket(),clientControl.getOis() );
                 checkMess.start();
             }
         }       
