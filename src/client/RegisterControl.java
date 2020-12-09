efa28fec190a446a3a79db5a8881369ca796d925
LamKzDev <lamitptit99@gmail.com>
2020-12-10 00:22:14 +0700

    update gameplay v11

--- src/client/RegisterControl.java
+++ src/client/RegisterControl.java
@@ -13,6 +13,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import model.Account;
 import model.Message;
+import model.User;
+import ui.ListFrm;
+import ui.LoginFrm;
 import ui.RegisterFrm;
 
 /**
@@ -20,6 +23,9 @@ import ui.RegisterFrm;
  * @author BENH VIEN CONG NGHE
  */
 public class RegisterControl {
+    Account account = new Account();
+    Message mesRecei = new Message();
+    private ListFrm listFrm;
     private RegisterFrm registerFrm;
     private ClientControl clientControl;
     public RegisterControl(RegisterFrm registerFrm, ClientControl clientControl){
@@ -46,5 +52,11 @@ public class RegisterControl {
     }
     public void showMessageSuccess(){
         registerFrm.showMessage("Register Success");
+        Message mesReq = new Message(account, Message.MesType.GET_SCOREBOARD);
+        clientControl.sendData(mesReq);
+        listFrm = new ListFrm();
+        InviteControl inviteControl = new InviteControl(clientControl, listFrm);
+        inviteControl.setUser((User) mesRecei.getObject());
+        listFrm.setVisible(true);
     }
 }
